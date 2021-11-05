c81b2cfbccb970c4da566cb98c5cd05e1a5d03ee
FutuZer <71083207+FutureZer@users.noreply.github.com>
2021-11-06 00:05:27 +0300

    Add files via upload

---
+++ HW_3/container.py
@@ -0,0 +1,45 @@
+import string
+from cryptographer import *
+
+
+class Container:
+    def __init__(self):
+        self.messages = []
+
+    # При инициализации контейнера информацией из файла
+    def file_init(self, messages):
+        for index in range(len(messages)):
+            cr = Crypter()
+            cr.file_init(messages[index])
+            self.messages.append(cr)
+
+    # При инициализации контейнера случайными сообщениями
+    def rnd_init(self, length):
+        for index in range(length):
+            cr = Crypter()
+            cr.rnd_init()
+            self.messages.append(cr)
+
+    # Печатает всю информации о сообщениях в контейнере в файл
+    def write(self, outstream):
+        outstream.write("======================================================================\n")
+        for message in self.messages:
+            message.write(outstream)
+            outstream.write("======================================================================\n")
+        pass
+
+    # Находит среднее значение кода сообщения
+    def average(self):
+        total = 0
+        for mes in self.messages:
+            total += mes.get_message_code()
+        return total / len(self.messages)
+
+    # Удаляет все сообщения контейнера, значение кода которых меньше среднего
+    def average_only(self):
+        aver = []
+        for mes in self.messages:
+            if mes.get_message_code() >= self.average():
+                aver.append(mes)
+        self.messages = aver
+        pass
---
+++ HW_3/cryptographer.py
@@ -0,0 +1,79 @@
+import random
+import string
+
+
+class Crypter:
+    # symb и crypt списки для шифрования подменой одного символа на другой
+    symb = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g',
+            'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o',
+            'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w',
+            'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    crypt = ['R', 'r', '2', '3', 'S', 's', 'T', 'u', 'U', 't', 'V', 'W', 'v', 'w',
+             '0', 'b', 'B', '9', 'c', 'y', 'z', 'C', 'X', 'Y', 'Z', 'x', 'F', 'f', 'I', 'i',
+             'A', 'G', 'g', 'a', 'P', 'q', 'p', 'Q', 'j', 'k', 'K', 'J', 'L', 'M', 'N', 'n',
+             'l', 'm', 'D', 'd', 'E', 'e', '5', '4', '1', 'O', '6', '7', 'o', 'H', 'h', '8']
+    rand = random.Random()
+
+    # Конструктор используется при инициализации через файл
+    def __init__(self):
+        self.message = []
+
+    # Метод используется при инициализации через файл
+    def file_init(self, mes):
+        self.message = mes
+
+    # Метод используется при инициализации сообщения случайным
+    def rnd_init(self):
+        self.message = ""
+        length = Crypter.rand.randint(3, 200)
+        for i in range(length):
+            if Crypter.rand.randint(0, 8) == 8 and i != 0:
+                self.message += ' '
+            else:
+                self.message += Crypter.symb[Crypter.rand.randint(0, 61)]
+
+    # Шифрует подменой одного символа на другой
+    def pair_crypt(self):
+        crypted = ""
+        for symbol in self.message:
+            for index in range(len(Crypter.symb)):
+                if symbol == ' ':
+                    crypted += ' '
+                    break
+                elif Crypter.symb[index] == symbol:
+                    crypted += Crypter.crypt[index]
+                    break
+        return crypted
+
+    # Шифрует сдвигом кода символа на небольшую случайную величину
+    def shift_crypt(self):
+        crypted = ""
+        shift = Crypter.rand.randint(-5, 5)
+        if shift == 0:
+            shift = 1
+        for symbol in self.message:
+            crypted += chr(ord(symbol) + shift)
+        return crypted
+
+    # Шифрует заменой символа на его код
+    def numeric_crypt(self):
+        crypt = ""
+        for symbol in self.message:
+            crypt += str(ord(symbol))
+        return crypt
+
+    # Возвращает код строки (среднее кодов всех символов)
+    def get_message_code(self):
+        total = 0
+        for symbol in self.message:
+            total += ord(symbol)
+        return int(total / len(self.message))
+
+    # Записывает информацию о сообщении в файл
+    def write(self, outstream):
+        outstream.write(" Message text: {}\n".format(self.message))
+        outstream.write(" Message code: {}\n".format(self.get_message_code()))
+        outstream.write("   Pair crypt: {}\n".format(self.pair_crypt()))
+        outstream.write("  Shift crypt: {}\n".format(self.shift_crypt()))
+        outstream.write("Numeric crypt: {}\n".format(self.numeric_crypt()))
+        pass
